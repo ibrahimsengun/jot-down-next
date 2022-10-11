707abc5a9c5de8b3f30b690f0eba8518c54bf037
Ibrahim <ibrahimsengun173@gmail.com>
2022-10-12 00:39:43 +0300

    description alani eklendi

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { BsCheckCircle, BsTrash } from "react-icons/bs";
 import { AiOutlineEdit, AiOutlineSave } from "react-icons/ai";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 import Button from "../Button";
 import { useTodo } from "../../context/TodoContext";
 import { ITodo } from "../../models/Todo";
@@ -11,8 +12,19 @@ interface ITodoItem {
 
 const TodoItem: React.FC<ITodoItem> = ({ todo }) => {
   const { _id, title, description, isCompleted } = todo;
-
   const { completeTodo, removeTodo } = useTodo();
+
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  const arrowIcon = isExpanded ? (
+    <MdKeyboardArrowUp size="1.5rem" />
+  ) : (
+    <MdKeyboardArrowDown size="1.5rem" />
+  );
+
+  const expandClickHandler = () => {
+    setIsExpanded((prev) => !prev);
+  };
 
   return (
     <div
@@ -56,8 +68,18 @@ const TodoItem: React.FC<ITodoItem> = ({ todo }) => {
               title="Delete"
             />
           </div>
+          <div>
+            <Button
+              icon={arrowIcon}
+              className="ml-2 hover:bg-transparent active:bg-transparent"
+              variant="borderless"
+              title="Description"
+              onClick={() => setIsExpanded((prev) => !prev)}
+            />
+          </div>
         </div>
       </div>
+      {isExpanded && <div className="px-2 py-3">{description}</div>}
     </div>
   );
 };
